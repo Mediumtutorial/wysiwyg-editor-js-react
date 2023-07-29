7aaa35b30683980d7366f030503d0dd88d0d7f64
Manish Mandal <manishmandal@Manishs-MacBook-Air.local>
2023-07-29 20:33:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Editor from "./Editor";
+
+// Initial Data
+const INITIAL_DATA = {
+  time: new Date().getTime(),
+  blocks: [
+    {
+      type: "header",
+      data: {
+        text: "This is my awesome editor!",
+        level: 1,
+      },
+    },
+  ],
+};
 
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="editor">
+      <Editor data={data} onChange={setData} editorblock="editorjs-container" />
+      <button
+        className="savebtn"
+        onClick={() => {
+          alert(JSON.stringify(data));
+        }}
+      >
+        Save
+      </button>
     </div>
   );
 }
